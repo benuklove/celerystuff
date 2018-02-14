c5d11f1aae6973298e1e8366fa5b2d1c0d8bdd22
benlove <benuklove@gmail.com>
2018-02-13 22:22:53 -0600

    Add a couple more tasks

--- proj/tasks.py
+++ proj/tasks.py
@@ -1,9 +1,17 @@
-from celery import Celery
-
-# app = Celery('tasks', broker='pyamqp://guest@localhost//')
-app = Celery('tasks', backend='redis://localhost', broker='pyamqp://')
+from __future__ import absolute_import, unicode_literals
+from .celery import app
 
 
 @app.task
 def add(x, y):
     return x + y
+
+
+@app.task
+def mul(x, y):
+    return x * y
+
+
+@app.task
+def xsum(numbers):
+    return sum(numbers)
