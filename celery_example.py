43b22c6da6d477e2ee515929dd55925e905138f3
benlove <benuklove@gmail.com>
2018-02-14 22:06:17 -0600

    Flask Celery example working
    Celery worker in a Flask app.
    RabbitMQ as broker and Redis as backend.
    Reverses string from url slug.

--- celery_example.py
+++ celery_example.py
@@ -4,13 +4,16 @@ from flask_celery import make_celery
 
 app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = 'amqp://localhost//'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost//'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://'
 
 celery = make_celery(app)
 
 @app.route('/process/<name>')
 def process(name):
-    return name
+
+    reverse.delay(name)
+
+    return 'I sent an async request!'
 
 @celery.task(name='celery_example.reverse')
 def reverse(string):
